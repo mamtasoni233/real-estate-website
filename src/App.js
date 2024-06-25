ed670cb192384c86de90fd73736083a924cff2cf
Vishal Kumar Khatri <kishore@bunity.com>
2024-06-25 19:00:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Features from './Components/Feature';
+import Sales from './Components/Onsale';
+import About from './Components/About';
 
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Page404 from './Components/Page404';
+let AllRoutes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'about-us',
+    element: <About />,
+  },
+  {
+    path: 'features',
+    element: <Features />,
+  },
+  {
+    path: 'sales',
+    element: <Sales />,
+  },
+  {
+    path: '*',
+    element: <Page404 />,
+  },
+]);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <RouterProvider router={AllRoutes} />
+      <Footer />
     </div>
   );
 }
